a53bff422d2cf62aa6c8d5b96fbb5cb0a476d7d3
Aluno <Aluno@SN-0329904>
2018-10-11 19:39:15 -0300

    Commit inicial

---
+++ src/java/br/com/senai/sout/model/Usuario.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senai.sout.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Aluno
+ */
+@Entity
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String login;
+    @Column
+    private String senha;
+    @Column
+    private String nome;
+    @Column
+    private String telefone;
+    @Column
+    private String email;
+    @Column
+    private String respostaSeguranca;
+    @Column
+    private String ieAdmin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRespostaSeguranca() {
+        return respostaSeguranca;
+    }
+
+    public void setRespostaSeguranca(String respostaSeguranca) {
+        this.respostaSeguranca = respostaSeguranca;
+    }
+
+    public String getIeAdmin() {
+        return ieAdmin;
+    }
+
+    public void setIeAdmin(String ieAdmin) {
+        this.ieAdmin = ieAdmin;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.id;
+        hash = 83 * hash + Objects.hashCode(this.login);
+        hash = 83 * hash + Objects.hashCode(this.senha);
+        hash = 83 * hash + Objects.hashCode(this.nome);
+        hash = 83 * hash + Objects.hashCode(this.telefone);
+        hash = 83 * hash + Objects.hashCode(this.email);
+        hash = 83 * hash + Objects.hashCode(this.respostaSeguranca);
+        hash = 83 * hash + Objects.hashCode(this.ieAdmin);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.login, other.login)) {
+            return false;
+        }
+        if (!Objects.equals(this.senha, other.senha)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.telefone, other.telefone)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.respostaSeguranca, other.respostaSeguranca)) {
+            return false;
+        }
+        if (!Objects.equals(this.ieAdmin, other.ieAdmin)) {
+            return false;
+        }
+        return true;
+    }
+    
+
+}
