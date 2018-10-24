ff0c53a613d4990e190e672f2f2c0d537cc518e2
Eduardo-PC <Eduardo-PC@DESKTOP-STMKK3U>
2018-10-24 18:23:19 -0200

    Atualizado SQL

--- src/java/br/com/senai/sout/dao/UsuarioDao.java
+++ src/java/br/com/senai/sout/dao/UsuarioDao.java
@@ -13,33 +13,44 @@ import java.util.List;
  * @author Aluno
  */
 public class UsuarioDao {
-        public void salvar(Usuario u) {
-        if(u.getId() == 0) {
-            Repository.inserir(u);            
+
+    public void salvar(Usuario u) {
+        if (u.getId() == 0) {
+            Repository.inserir(u);
         } else {
             Repository.alterar(u);
         }
         Repository.commitar();
     }
-    
+
     public void excluir(Usuario u) {
         Repository.excluir(u);
         Repository.commitar();
     }
-    
+
     public List<Usuario> buscarTodos() {
         return Repository.getEm().
-                createQuery("select u from Usuario u").getResultList();
+                createQuery("select u from tb_usuario u").getResultList();
     }
-    
+
     public Usuario buscaId(int id) {
         return Repository.getEm().find(Usuario.class, id);
     }
-    
+    public void limpaTabelaUsuario(){
+        Repository.getEm().
+                createQuery("delete from tb_usuario where 1=1");
+        Repository.commitar();
+    }
+
     public Usuario buscaNome(String nome) {
         return (Usuario) Repository.getEm().
-                createQuery("select u from Usuario u where "
+                createQuery("select u from tb_usuario u where "
                         + "u.nome like :nome").setParameter("nome", "%" + nome + "%")
+                .getSingleResult();
+    }
+    public Long getQuantidadeRegistros(){
+        return (Long) Repository.getEm().
+                createQuery("select count(u) qtd_registros from tb_usuario u")
                 .getSingleResult();
     }
 }
--- src/java/br/com/senai/sout/model/Imagem.java
+++ src/java/br/com/senai/sout/model/Imagem.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 
 /**
  *
@@ -20,17 +21,17 @@ import javax.persistence.Id;
 public class Imagem implements Serializable {
     @Id
     @GeneratedValue(strategy= GenerationType.SEQUENCE)
-    private int id;
+    private Integer id;
     @Column
     private String caminho;
-    @Column
+    @JoinColumn()
     private Info info;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
